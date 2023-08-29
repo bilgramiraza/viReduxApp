296127f166d315ce221327afeb2bffb8def091a2
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-29 22:20:39 +0530

    Added Dependency:Redux

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,10 @@
+import { combineReducers } from "redux";
 import filtersReducer from "./features/filters/filtersSlice";
 import todosReducer from "./features/todos/todosSlice";
 
-function rootReducer(state = {}, action) {
-  return {
-    todos:todosReducer(state.todos,action),
-    filters:filtersReducer(state.filters,action),
-  };
-}
+const rootReducer = combineReducers({
+    todos:todosReducer,
+    filters:filtersReducer,
+});
 
 export default rootReducer;
