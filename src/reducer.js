f927c1353bc06a3bc015a0659a3d1f333d4b0723
bilgramiraza <bilgramiraza@yahoo.com>
2023-08-29 21:03:23 +0530

    Added More Actions to the reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,35 +1,48 @@
-function nextTodoId(todos){
-  const maxId = todos.reduce((maxId,todo)=>Math.max(todo.id,maxId),-1);
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
   return maxId;
 }
 
 const initalState = {
-  todos:[
-    {id:0, text:'Learn React', completed:true},
-    {id:1, text:'Learn Redux', completed:false, color:'purple' },
-    {id:2, text:'Build Fun Stuff', completed:false, color:'blue'},
+  todos: [
+    { id: 0, text: 'Learn React', completed: true },
+    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+    { id: 2, text: 'Build Fun Stuff', completed: false, color: 'blue' },
   ],
-  filters:{
-    status:'All',
-    colors:[],
+  filters: {
+    status: 'All',
+    colors: [],
   },
 };
 
-function appReducer(state = initalState, action){
-  switch(action.type){
+function appReducer(state = initalState, action) {
+  switch (action.type) {
     case 'todos/todoAdded':
       return {
         ...state,
-        todos:[
+        todos: [
           ...state.todos,
           {
-            id:nextTodoId(state.todos),
-            text:action.payload,
-            completed:false,
+            id: nextTodoId(state.todos),
+            text: action.payload,
+            completed: false,
           },
         ],
       };
-    default: 
+    case 'todos/todoToggled':
+      return {
+        ...state,
+        todos: state.todos.map(todo => (todo.id === action.payload) ? todo : { ...todo, completed: !todo.completed }),
+      };
+    case 'filters/statusFilterChanged':
+      return {
+        ...state,
+        filters:{
+          ...state.filters,
+          status:action.payload,
+        },
+      };
+    default:
       return state;
   }
 }
