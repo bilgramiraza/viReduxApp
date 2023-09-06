8761aec181db4ba7058dae8c17442b252b21ab50
bilgramiraza <bilgramiraza@yahoo.com>
2023-09-06 16:07:10 +0530

    Added Loading State to Header Input section

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -4,22 +4,29 @@ import { saveNewTodo } from "../todos/todosSlice";
 
 const Header = () => {
   const [text, setText] = useState('');
+  const [status, setStatus] = useState('idle');
   const dispatch = useDispatch();
 
   const handleChange = (e) => setText(e.target.value);
 
-  const handleSubmit = (e)=>{
-    if(e.key !== 'Enter')  return;
-    const trimmedText = e.target.value.trim();
-    if(trimmedText){
-      dispatch(saveNewTodo(trimmedText));
+  const handleSubmit = async (e)=>{
+    const trimmedText = text.trim();
+    if(e.key === 'Enter' && trimmedText){
+      setStatus('loading');
+      await dispatch(saveNewTodo(trimmedText));
       setText('');
+      setStatus('idle');
     }
   };
 
+  let isLoading = status === 'loading';
+  let placeHolder = isLoading ? '' : 'What we gotta do?';
+  let loader = isLoading ? <div className="loader" /> : null;
+
   return (
     <header className="header">
-      <input className="new-todo" placeholder="What we gotta do?" autoFocus={true} value={text} onChange={handleChange} onKeyDown={handleSubmit} />
+      <input className="new-todo" placeholder={placeHolder} autoFocus={true} value={text} onChange={handleChange} onKeyDown={handleSubmit} disabled={isLoading}/>
+      {loader}
     </header>
   );
 };
